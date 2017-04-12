62e0d90893c44f32cdb42b45916daa1f3497900b
Adapix <adapix.contact@gmail.com>
2017-04-12 15:14:04 +0200

    Adjust footer

--- vues/static/footer.php
+++ vues/static/footer.php
@@ -1,36 +1,41 @@
-<div id="footer">
-	<div class="block_center">
-		<div class="bloc_footer">
-			<h2>Participer</h2>
-			<ul>
-				<a class="a_underline" href="#"><li>Proposer des produits</li></a>
-				<a class="a_underline" href="#"><li>Transformer</li></a>
-				<a class="a_underline" href="#"><li>Devenir client</li></a>
-				<a class="a_underline" href="#"><li>Distribuer</li></a>
-			</ul>
+<footer id="footer" class="footer">
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h2>Participer</h2>
+					<ul>
+						<a class="a_underline" href="#"><li>Proposer des produits</li></a>
+						<a class="a_underline" href="#"><li>Transformer</li></a>
+						<a class="a_underline" href="#"><li>Devenir client</li></a>
+						<a class="a_underline" href="#"><li>Distribuer</li></a>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<h2>Comprendre</h2>
+					<ul>
+						<a class="a_underline" href="#"><li>Savoir faire local</li></a>
+						<a class="a_underline" href="#"><li>Réduire les transports</li></a>
+						<a class="a_underline" href="#"><li>Produits frais</li></a>
+						<a class="a_underline" href="#"><li>Qualité</li></a>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<h2>Communiquer</h2>
+					<ul>
+						<a class="a_underline" href="#"><li>Les secrets des producteurs</li></a>
+						<a class="a_underline" href="#"><li>Le savoir faire des artisants</li></a>
+						<a class="a_underline" href="#"><li>Les recettes de grand-mère</li></a>
+						<a class="a_underline" href="#"><li>La conservation des aliments</li></a>
+					</ul>
+				</div>	
+			</div>	
 		</div>
-		<div class="bloc_footer">
-			<h2>Comprendre</h2>
-			<ul>
-				<a class="a_underline" href="#"><li>Savoir faire local</li></a>
-				<a class="a_underline" href="#"><li>Réduire les transports</li></a>
-				<a class="a_underline" href="#"><li>Produits frais</li></a>
-				<a class="a_underline" href="#"><li>Qualité</li></a>
-			</ul>
-		</div>
-		<div class="bloc_footer">
-			<h2>Communiquer</h2>
-			<ul>
-				<a class="a_underline" href="#"><li>Les secrets des producteurs</li></a>
-				<a class="a_underline" href="#"><li>Le savoir faire des artisants</li></a>
-				<a class="a_underline" href="#"><li>Les recettes de grand-mère</li></a>
-				<a class="a_underline" href="#"><li>La conservation des aliments</li></a>
-			</ul>
+		
+		<div class="pull-right">
+			<a href="#">@copyright NW - 2017 |</a>
+			<a href="#">Nous contacter |</a>
+			<a href="#">mentions légales</a>
 		</div>	
-	</div>
-	<div class="mention">
-		<a href="#">@copyright NW - 2017 |</a>
-		<a href="#">Nous contacter |</a>
-		<a href="#">mentions légales</a>
-	</div>
-</div>
+	</div>	
+</footer>
