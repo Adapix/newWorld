a6ba33b916e723ca7cfeb9231755eafa62865089
Adapix <adapix.contact@gmail.com>
2017-04-12 16:02:15 +0200

    Update Navbar to responsive bootstrap technologie

--- vues/static/navigation.php
+++ vues/static/navigation.php
@@ -1,13 +1,25 @@
-<div class="navbar navigation">
-	<a href="index.php?page=index"><title id="logo">NW</title></a>
-	<a class="bouttonMenu" href="index.php?page=acheter">Acheter</a>
-	<a class="bouttonMenu" href="index.php?page=produire">Produire</a>
-	<a class="bouttonMenu" href="index.php?page=distribution">Distribuer</a>
-	<?php if (is_connected()){ ?> 
-	<a class="bouttonMenu" href="index.php?page=newLot">Ajouter un lot</a>
-	<?php } ?>
-	<form method="get" class="pull-right search_contnair" action="search.php">
-		<input class="search_button" type="search" name="search" placeholder="Rechercher">
-	</form>
-</div>
-	
+<nav class="navbar navbar-inverse navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="index.php?page=index"><title id="logo">NW</title></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="menu-onglet"><a href="index.php?page=acheter">Acheter</a></li>
+		<li class="menu-onglet"><a href="index.php?page=produire">Produire</a></li>
+		<li class="menu-onglet"><a href="index.php?page=distribution">Distribuer</a></li>
+		<?php if (is_connected()){ ?> 
+		<li><a href="index.php?page=newLot">Ajouter un lot</a></li>
+		<?php } ?>					
+      </ul>
+      <form method="get" class="pull-right search_contnair" action="search.php">
+							<input class="search_button" type="search" name="search" placeholder="Rechercher">
+					</form>
+    </div>	    
+  </div>
+</nav>
