f6554eba68575f07e211cf393dd0e3fe766b16b1
Adapix <adapix.contact@gmail.com>
2017-04-12 14:42:01 +0200

    Adjust text and images position and responsive

--- vues/index.php
+++ vues/index.php
@@ -1,13 +1,19 @@
-<div class="content" >
-	<section id="description" class="text_accueil col-md-8">
-		<p class="">Les meilleurs produits de saison.</p>
-		<p class="">Du producteur à l'artisanat et au consommateur.</p>
-		<p class="">Ni usine, ni camion, ni grande surface.</p>
-		<br><p class="">La terre et l'homme à nouveau respectés.</p><br>
-		<p class="">New World</p>
-	</section>
-	<section class="col-md-4">
-		<img  class="img-responsive image_accueil" src="images/jardinier-a-larrosoir.jpg">
-		<img  class="img-responsive image_accueil" src="images/boucher.jpg">
-	</section>
+<div class="content container-fluid" >
+	<div class="row-fluid">
+		<div id="description" class="text_accueil col-md-8 text-center">
+			<p class="">Les meilleurs produits de saison.</p>
+			<p class="">Du producteur à l'artisanat et au consommateur.</p>
+			<p class="">Ni usine, ni camion, ni grande surface.</p>
+			<br><p class="">La terre et l'homme à nouveau respectés.</p><br>
+			<p class="">New World</p>
+		</div>
+		<div class="col-md-4">
+			<div class="image_accueil">
+				<img  class="img-responsive" src="images/jardinier-a-larrosoir.jpg">
+				<br>
+				<img  class="img-responsive" src="images/boucher.jpg">
+				<br>
+			</div>			
+		</div>
+	</div>
 </div>
