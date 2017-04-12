a43532f733ae2199dab8d8d98026eb3d0e4361c1
Adapix <adapix.contact@gmail.com>
2017-04-12 13:56:27 +0200

    Adjust text_accueil

--- vues/index.php
+++ vues/index.php
@@ -1,10 +1,10 @@
 <div class="content" >
-	<section class="col-md-8">
-		<p class="text_accueil">Les meilleurs produits de saison.</p>
-		<p class="text_accueil">Du producteur à l'artisanat et au consommateur.</p>
-		<p class="text_accueil">Ni usine, ni camion, ni grande surface.</p>
-		<br><p class="text_accueil">La terre et l'homme à nouveau respectés.</p><br>
-		<p class="text_accueil">New World</p>
+	<section class="text_accueil col-md-8">
+		<p class="">Les meilleurs produits de saison.</p>
+		<p class="">Du producteur à l'artisanat et au consommateur.</p>
+		<p class="">Ni usine, ni camion, ni grande surface.</p>
+		<br><p class="">La terre et l'homme à nouveau respectés.</p><br>
+		<p class="">New World</p>
 	</section>
 	<section class="col-md-4">
 		<img  class="image_accueil" src="images/jardinier-a-larrosoir.jpg">
