ae171e97ade9b17311e50bb013004f2150d67f6a
Adapix <adapix.contact@gmail.com>
2017-04-12 15:23:12 +0200

    Add jquery call in head

--- vues/static/head.php
+++ vues/static/head.php
@@ -5,4 +5,5 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 </head>
