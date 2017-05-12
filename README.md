ac88d62081d9bdc3949486ad5e8b990e4e8960ad
Adapix <adapix.contact@gmail.com>
2017-05-12 22:05:54 +0200

    Update script SQL and Readme.md

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # newWorld
 Projet étudiant BTS SIO
 French Student Project
+
+Le script SQL de la base est inclus.
+
+Pour rendre la connexion fonctionnelle il faut éditer de fichier index.php à la racine du répertoire et changer les informations de connexion à la base de donnée ne fonction de vos paramètre.
+
+Parmis les nouvelles fonctionalités de cette version :
+Une amélioration du responsive du site 
+			-> Le menu principal s'adapte désormais à l'affichage sur téléphone
+
+
--- newWorld.sql
+++ newWorld.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u2
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost
--- Généré le :  Mer 26 Avril 2017 à 16:00
--- Version du serveur :  5.5.52-0+deb8u1
--- Version de PHP :  5.6.29-0+deb8u1
+-- Client :  127.0.0.1
+-- Généré le :  Ven 12 Mai 2017 à 20:02
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,20 +14,20 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `newWorld`
+-- Base de données :  `newworld`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `lieuDeProd`
+-- Structure de la table `lieudeprod`
 --
 
-CREATE TABLE IF NOT EXISTS `lieuDeProd` (
-`idLieuDeProduction` int(11) NOT NULL,
+CREATE TABLE `lieudeprod` (
+  `idLieuDeProduction` int(11) NOT NULL,
   `commune` varchar(45) DEFAULT NULL,
   `parcelle` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -38,8 +38,8 @@ CREATE TABLE IF NOT EXISTS `lieuDeProd` (
 -- Structure de la table `lot`
 --
 
-CREATE TABLE IF NOT EXISTS `lot` (
-`idLot` int(11) NOT NULL,
+CREATE TABLE `lot` (
+  `idLot` int(11) NOT NULL,
   `poids` int(11) DEFAULT NULL,
   `aliment` enum('abricots','bananes','fraises','pêches','framboises','haricots vert','laitues','myrtilles','poivrons','pommes de terre') DEFAULT NULL,
   `modeDeProduction` enum('conventionnel','biologique','raisonné') DEFAULT NULL,
@@ -52,25 +52,16 @@ CREATE TABLE IF NOT EXISTS `lot` (
   `idRetrait` int(11) DEFAULT NULL,
   `idProducteur` int(11) DEFAULT NULL,
   `nomArticle` varchar(80) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `lot`
---
-
-INSERT INTO `lot` (`idLot`, `poids`, `aliment`, `modeDeProduction`, `idLieuDeProduction`, `dateRecolte`, `dlc`, `qte`, `prixAuKg`, `qteMinParVente`, `idRetrait`, `idProducteur`, `nomArticle`) VALUES
-(1, NULL, 'abricots', 'biologique', NULL, '03/02/17', '05/03/17', 125, 1.85, 2, NULL, NULL, 'Les pommes rambo'),
-(2, NULL, 'laitues', 'biologique', NULL, '15/03//17', '20/03/17', 20, 8, 2, NULL, NULL, 'Iceberg'),
-(3, NULL, 'fraises', 'biologique', NULL, '05/04/17', '20/04/17', 20, 3, 1, NULL, NULL, 'Guariguettes');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `pointDeRetrait`
+-- Structure de la table `pointderetrait`
 --
 
-CREATE TABLE IF NOT EXISTS `pointDeRetrait` (
-`idPointDeRetrait` int(11) NOT NULL,
+CREATE TABLE `pointderetrait` (
+  `idPointDeRetrait` int(11) NOT NULL,
   `numero` varchar(45) DEFAULT NULL,
   `ville` varchar(45) DEFAULT NULL,
   `codePostal` varchar(45) DEFAULT NULL,
@@ -84,8 +75,8 @@ CREATE TABLE IF NOT EXISTS `pointDeRetrait` (
 -- Structure de la table `producteur`
 --
 
-CREATE TABLE IF NOT EXISTS `producteur` (
-`idProducteur` int(11) NOT NULL,
+CREATE TABLE `producteur` (
+  `idProducteur` int(11) NOT NULL,
   `nom` varchar(45) DEFAULT NULL,
   `prenom` varchar(45) DEFAULT NULL,
   `identifiant` varchar(45) DEFAULT NULL,
@@ -99,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `producteur` (
 -- Structure de la table `retrait`
 --
 
-CREATE TABLE IF NOT EXISTS `retrait` (
+CREATE TABLE `retrait` (
   `idRetrait` int(11) NOT NULL,
   `idPointDeRetrait` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -110,91 +101,87 @@ CREATE TABLE IF NOT EXISTS `retrait` (
 -- Structure de la table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-`idUser` bigint(20) NOT NULL,
+CREATE TABLE `users` (
+  `idUser` bigint(20) NOT NULL,
   `nomUser` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT '0',
   `signup_date` int(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `users`
---
-
-INSERT INTO `users` (`idUser`, `nomUser`, `password`, `email`, `admin`, `signup_date`) VALUES
-(1, 'Nicolas', 'lecode01', 'lemail@mail.fr', 1, 7032017);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Index pour les tables exportées
 --
 
 --
--- Index pour la table `lieuDeProd`
+-- Index pour la table `lieudeprod`
 --
-ALTER TABLE `lieuDeProd`
- ADD PRIMARY KEY (`idLieuDeProduction`);
+ALTER TABLE `lieudeprod`
+  ADD PRIMARY KEY (`idLieuDeProduction`);
 
 --
 -- Index pour la table `lot`
 --
 ALTER TABLE `lot`
- ADD PRIMARY KEY (`idLot`), ADD KEY `fk_lot_1_idx` (`idLieuDeProduction`), ADD KEY `fk_lot_2_idx` (`idProducteur`);
+  ADD PRIMARY KEY (`idLot`),
+  ADD KEY `fk_lot_1_idx` (`idLieuDeProduction`),
+  ADD KEY `fk_lot_2_idx` (`idProducteur`);
 
 --
--- Index pour la table `pointDeRetrait`
+-- Index pour la table `pointderetrait`
 --
-ALTER TABLE `pointDeRetrait`
- ADD PRIMARY KEY (`idPointDeRetrait`);
+ALTER TABLE `pointderetrait`
+  ADD PRIMARY KEY (`idPointDeRetrait`);
 
 --
 -- Index pour la table `producteur`
 --
 ALTER TABLE `producteur`
- ADD PRIMARY KEY (`idProducteur`);
+  ADD PRIMARY KEY (`idProducteur`);
 
 --
 -- Index pour la table `retrait`
 --
 ALTER TABLE `retrait`
- ADD PRIMARY KEY (`idRetrait`,`idPointDeRetrait`), ADD KEY `fk_retrait_2_idx` (`idPointDeRetrait`);
+  ADD PRIMARY KEY (`idRetrait`,`idPointDeRetrait`),
+  ADD KEY `fk_retrait_2_idx` (`idPointDeRetrait`);
 
 --
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`idUser`);
+  ADD PRIMARY KEY (`idUser`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT pour la table `lieuDeProd`
+-- AUTO_INCREMENT pour la table `lieudeprod`
 --
-ALTER TABLE `lieuDeProd`
-MODIFY `idLieuDeProduction` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `lieudeprod`
+  MODIFY `idLieuDeProduction` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `lot`
 --
 ALTER TABLE `lot`
-MODIFY `idLot` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `idLot` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT pour la table `pointDeRetrait`
+-- AUTO_INCREMENT pour la table `pointderetrait`
 --
-ALTER TABLE `pointDeRetrait`
-MODIFY `idPointDeRetrait` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pointderetrait`
+  MODIFY `idPointDeRetrait` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `producteur`
 --
 ALTER TABLE `producteur`
-MODIFY `idProducteur` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProducteur` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-MODIFY `idUser` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+  MODIFY `idUser` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- Contraintes pour les tables exportées
 --
@@ -203,15 +190,15 @@ MODIFY `idUser` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 -- Contraintes pour la table `lot`
 --
 ALTER TABLE `lot`
-ADD CONSTRAINT `fk_lot_1` FOREIGN KEY (`idLieuDeProduction`) REFERENCES `lieuDeProd` (`idLieuDeProduction`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_lot_2` FOREIGN KEY (`idProducteur`) REFERENCES `producteur` (`idProducteur`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_lot_1` FOREIGN KEY (`idLieuDeProduction`) REFERENCES `lieudeprod` (`idLieuDeProduction`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_lot_2` FOREIGN KEY (`idProducteur`) REFERENCES `producteur` (`idProducteur`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Contraintes pour la table `retrait`
 --
 ALTER TABLE `retrait`
-ADD CONSTRAINT `fk_retrait_1` FOREIGN KEY (`idRetrait`) REFERENCES `lot` (`idLot`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_retrait_2` FOREIGN KEY (`idPointDeRetrait`) REFERENCES `pointDeRetrait` (`idPointDeRetrait`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_retrait_1` FOREIGN KEY (`idRetrait`) REFERENCES `lot` (`idLot`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_retrait_2` FOREIGN KEY (`idPointDeRetrait`) REFERENCES `pointderetrait` (`idPointDeRetrait`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
